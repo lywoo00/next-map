5fa739319bae94625f55d768cbfe5043ebc30f5c
lee <xmsxms000@gmail.com>
2025-05-08 16:41:18 +0900

    prismaClient ts파일 분리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import StoreBox from "@/components/StoreBox";
 import { StoreType } from "@/interface";
 import { useState } from "react";
 import axios from "axios";
-export default function Home({stores}: {stores: StoreType[]}) {
+export default function Home({ stores }: { stores: StoreType[] }) {
   const [map, setMap] = useState(null);
   const [currentStore, setCurrentStore] = useState(null);
   // const storeDatas = stores["DATA"];
@@ -24,11 +24,11 @@ export default function Home({stores}: {stores: StoreType[]}) {
   );
 }
 
-export async function getStaticProps(){
-const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`)
+// export async function getStaticProps() {
+//   const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
 
-return {
-  props: {stores: stores.data},
-  revalidate: 60 * 60
-}
-}
+//   return {
+//     props: { stores: stores.data },
+//     revalidate: 60 * 60,
+//   };
+// }
